bb4cc7ebbc4820c3e0436e4188fff518c78b66a2
Ros-Indigo <madan.mana1@gmail.com>
2019-12-14 04:58:50 -0800

    Writing to the file done

--- Testing_Tools/ExtractNouns.py
+++ Testing_Tools/ExtractNouns.py
@@ -5,4 +5,13 @@ import textblob
 opentext = open("Sample.txt")
 readtext = opentext.read()
 blob = textblob.TextBlob(readtext)
+#open file for writing
+openfile = open("Results.txt","w")
+for i in blob.noun_phrases:
+ openfile.write(i)
+ openfile.write(" ")
+ openfile.write(",")
+
+openfile.close()
+
 print(blob.noun_phrases)
