8b8b2da328a5146689b18f2a08fd06d4c53c6fd7
Ros-Indigo <madan.mana1@gmail.com>
2019-12-18 07:42:14 -0800

    Set Difference

--- Experiment2/StringComparision.cpp
+++ Experiment2/StringComparision.cpp
@@ -2,16 +2,17 @@
 #include<iostream>
 #include<map>
 #include<fstream>
+#include<algorithm>
 #define ll long long int
 using namespace std;
 map<string,int> m;
-set<string> s;
-
+set<string> student_notes;
+set<string> teacher_notes;
 
 int main()
 {
 //Read the strings from the index file and put into hashmap
-ifstream fin; 
+ifstream fin;
 fin.open("Results(Mam).txt");
 string line;
 cout<<"Outside the while loop"<<endl;
@@ -34,6 +35,7 @@ count++;
 cout<<"Putting into hashmap"<<endl;
 cout<<temp<<endl;
 m[temp]=1;
+teacher_notes.insert(temp);
 i++;
 }
 
@@ -42,7 +44,7 @@ fin.close();
 cout<<"Closed index file"<<endl;
 cout<<"Opening the Notes file"<<endl;
 ifstream fin2;
-fin2.open("Results(Gaurav).txt");
+fin2.open("Results(Aditya).txt");
 string line2;
 getline(fin2,line2);
 cout<<line2<<endl;
@@ -61,12 +63,13 @@ temp2 = temp2 + line2[j];
 j++;
 }
 cout<<temp2<<endl;
+student_notes.insert(temp2);
 count2++;
 
 if(m[temp2]!=0){
 cout<<"Found"<<endl;
 match++;
-s.insert(temp2);
+//s.insert(temp2);
 }else
 cout<<"Not Found"<<endl;
 j++;
@@ -74,17 +77,28 @@ j++;
 cout<<"Total String for Index file found"<<" "<<count<<endl;
 cout<<"Total String for Notes File found"<<" "<<count2<<endl;
 cout<<"Matches Found"<<" "<<match<<endl;
-cout<<"Distinct Matches Found"<<" "<<s.size()<<" "<<endl;
+//cout<<"Distinct Matches Found"<<" "<<s.size()<<" "<<endl;
 cout<<endl;
-std::set<std::string>::iterator it= s.begin();
- 
+//std::set<std::string>::iterator it= s.begin();
+
 // Iterate till the end of set
-while (it != s.end())
+/*while (it != s.end())
 {
 	// Print the element
 	std::cout << (*it) << " , ";
 	//Increment the iterator
 	it++;
 }
+*/
+cout<<"Finding the difference between the sets"<<endl;
 
+set<string> result;
+set_difference(teacher_notes.begin(),teacher_notes.end(),student_notes.begin(),student_notes.end(),std::insert_iterator<set<string> >(result, result.end()));
+std::set<std::string>::iterator it= result.begin();
+cout<<"Total topics in Teachers-Student Notes ----------------"<<result.size()<<"\n";
+while(it!=result.end())
+{
+  cout<<(*it)<<" , ";
+  it++;
+}
 }
