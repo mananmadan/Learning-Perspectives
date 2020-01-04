2ee6834bed27607e4ee25572da3b6b787a039ad8
mananmadan <madan.manan1@gmail.com>
2020-01-04 10:35:31 -0800

    Automatic Update

--- Notes_for_testing/modern_encryption/ExtractNouns.py
+++ Notes_for_testing/modern_encryption/ExtractNouns.py
@@ -6,6 +6,7 @@ import textblob
 opentext = open("Notes.txt")
 readtext = opentext.read()
 #print(readtext)
+
 lines = readtext
 #print(type(lines))
 
@@ -15,12 +16,15 @@ is_noun = lambda pos: pos[:2] == 'NN'
 # do the nlp stuff
 
 tokenized = nltk.word_tokenize(lines)
+
 print(tokenized)
+#tokens_without_punct_python = [t for t in tokenized if t not in string.punctuation]
+#print(tokens_without_punct_python)
 nouns = [word for (word, pos) in nltk.pos_tag(tokenized) if is_noun(pos)]
 
 print nouns
 
-openfile = open("Results(Ai)(Noun).txt","w")
+openfile = open("Results(Encryption)(Noun).txt","w")
 for i in nouns:
     openfile.write(i)
     openfile.write(",")
@@ -28,7 +32,7 @@ print("Done")
 blob = textblob.TextBlob(readtext)
 #open file for writing
 list=[]
-openfile = open("Results(Ai)(noun_phrases).txt","w")
+openfile = open("Results(Encryption)(noun_phrases).txt","w")
 for i in blob.noun_phrases:
  #openfile.write(" " ")
  openfile.write(i.decode('utf-8'))
