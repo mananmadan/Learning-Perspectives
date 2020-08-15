3ea1228e564587e941e0c3ad72c4ba9fc52f5cb8
Adtiya Aggarwal <dev.adityaaggarwal@gmail.com>
2020-08-15 20:06:23 +0530

    added Google links

---
+++ code/experimental/km.py
@@ -0,0 +1,25 @@
+import numpy as np
+from kmodes.kmodes import KModes
+
+# random categorical data
+
+for i in range(0,67):
+    datafile= open('').readlines()
+
+"""
+data = [
+["a","d","b","d"],
+["a","a","c","c"],
+["b","b","a","b"],
+["a","d","c","d"],
+["c","b","a","b"],
+["a","a","c","d"],
+]
+
+km = KModes(n_clusters=2, init='Huang', n_init=10, verbose=1)
+
+clusters = km.fit_predict(data)
+
+# Print the cluster centroids
+print(km.cluster_centroids_)
+"""
